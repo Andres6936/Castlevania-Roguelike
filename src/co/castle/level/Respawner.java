14348aaf3995a75136bfba44d2d4d80e0252a4d1
Joan Andrés <andres6936@live.com>
2020-11-07 11:00:13 -0500

    Use of final

--- src/co/castle/level/Respawner.java
+++ src/co/castle/level/Respawner.java
@@ -2,19 +2,16 @@ package co.castle.level;
 
 import co.castle.actor.Actor;
 
-public class Respawner extends Actor
-{
-	private int freq;
-	private int prob;
+public class Respawner extends Actor {
+	private final int freq;
+	private final int prob;
 
-	public Respawner( int freq, int prob )
-	{
+	public Respawner(int freq, int prob) {
 		this.freq = freq;
 		this.prob = prob;
 	}
 
-	public String getDescription( )
-	{
+	public String getDescription() {
 		return "Respawnie";
 	}
 
