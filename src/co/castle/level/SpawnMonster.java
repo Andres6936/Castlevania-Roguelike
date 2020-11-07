6d737bfd8865948d1e63fab2e799fc23f2eff2dd
Joan Andrés <andres6936@live.com>
2020-11-07 11:00:44 -0500

    Use of final

--- src/co/castle/level/SpawnMonster.java
+++ src/co/castle/level/SpawnMonster.java
@@ -3,18 +3,15 @@ package co.castle.level;
 import co.castle.action.Action;
 import sz.util.Util;
 
-public class SpawnMonster extends Action
-{
-	private static SpawnMonster singleton = new SpawnMonster( );
+public class SpawnMonster extends Action {
+	private static final SpawnMonster singleton = new SpawnMonster();
 
-	public static SpawnMonster getAction( )
-	{
+	public static SpawnMonster getAction() {
 		return singleton;
 	}
 
-	public void execute( )
-	{
-		Level level = performer.getLevel( );
+	public void execute() {
+		Level level = performer.getLevel();
 		Respawner perf = (Respawner) performer;
 		if ( Util.chance( perf.getProb( ) ) )
 			level.respawn( );
