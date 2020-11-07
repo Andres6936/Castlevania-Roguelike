62a8a16ca57bb02a26032cf19edce28e9d920feb
Joan Andrés <andres6936@live.com>
2020-11-07 10:59:25 -0500

    Use of final and generic

--- src/co/castle/level/MapCellFactory.java
+++ src/co/castle/level/MapCellFactory.java
@@ -4,19 +4,17 @@ import java.util.Hashtable;
 
 import co.castle.game.CRLException;
 
-public class MapCellFactory
-{
-	private Hashtable definitions;
-	private static MapCellFactory singleton = new MapCellFactory( );
+public class MapCellFactory {
+	private final Hashtable<String, Cell> definitions;
+	private static final MapCellFactory singleton = new MapCellFactory();
 
 	/*
 	 * public Cell buildMapCell (String id){ Cell x = (Cell) definitions.get(id);
 	 * return x.clone(); }
 	 */
 
-	public MapCellFactory( )
-	{
-		definitions = new Hashtable( 40 );
+	public MapCellFactory() {
+		definitions = new Hashtable<>(40);
 	}
 
 	public static MapCellFactory getMapCellFactory( )
@@ -29,18 +27,15 @@ public class MapCellFactory
 		definitions.put( definition.getID( ), definition );
 	}
 
-	public Cell getMapCell( String id ) throws CRLException
-	{
-		Cell ret = (Cell) definitions.get( id );
-		if ( ret != null )
+	public Cell getMapCell( String id ) throws CRLException {
+		Cell ret = definitions.get(id);
+		if (ret != null)
 			return ret;
-		throw new CRLException( "MapCellID " + id + " not found" );
+		throw new CRLException("MapCellID " + id + " not found");
 	}
 
-	public void init( Cell[ ] defs )
-	{
-		for ( int i = 0; i < defs.length; i++ )
-			definitions.put( defs[ i ].getID( ), defs[ i ] );
+	public void init( Cell[ ] defs ) {
+		for (Cell def : defs) definitions.put(def.getID(), def);
 	}
 
 }
