2007b72fa6ed5a83d9101e891b8b72dc75399d5f
Joan Andrés <andres6936@live.com>
2020-11-06 22:38:21 -0500

    Use of for-loop

--- src/co/castle/ai/monster/boss/DemonDraculaAI.java
+++ src/co/castle/ai/monster/boss/DemonDraculaAI.java
@@ -1,7 +1,5 @@
 package co.castle.ai.monster.boss;
 
-import java.util.Iterator;
-
 import co.castle.action.Action;
 import co.castle.action.ActionFactory;
 import co.castle.action.monster.MonsterCharge;
@@ -19,7 +17,6 @@ import sz.util.Util;
 
 public class DemonDraculaAI extends MonsterAI
 {
-	private int approachLimit = 0;
 	private int chargeCounter = 99;
 
 	public ActionSelector derive( )
@@ -75,48 +72,36 @@ public class DemonDraculaAI extends MonsterAI
 			else if ( rangedAttacks != null && Util.chance( 80 ) )
 			{
 				// Try to attack the player
-				for ( Iterator iter = rangedAttacks.iterator( ); iter.hasNext( ); )
-				{
-					RangedAttack element = (RangedAttack) iter.next( );
-					if ( element.getRange( ) >= playerDistance
-							&& Util.chance( element.getFrequency( ) ) )
-					{
+				for (RangedAttack element : rangedAttacks) {
+					if (element.getRange() >= playerDistance
+							&& Util.chance(element.getFrequency())) {
 						// Perform the attack
-						Action ret = ActionFactory.getActionFactory( )
-								.getAction( element.getAttackId( ) );
-						if ( element.getChargeCounter( ) > 0 )
-						{
-							if ( chargeCounter == 0 )
-							{
-								chargeCounter = element.getChargeCounter( );
-							}
-							else
-							{
+						Action ret = ActionFactory.getActionFactory()
+								.getAction(element.getAttackId());
+						if (element.getChargeCounter() > 0) {
+							if (chargeCounter == 0) {
+								chargeCounter = element.getChargeCounter();
+							} else {
 								chargeCounter--;
 								continue;
 							}
 						}
 
-						if ( ret instanceof MonsterMissile )
-						{
-							( (MonsterMissile) ret ).set( element.getAttackType( ),
-									element.getStatusEffect( ), element.getRange( ),
-									element.getAttackMessage( ), element.getEffectType( ),
-									element.getEffectID( ), element.getDamage( ),
-									element.getEffectWav( ) );
+						if (ret instanceof MonsterMissile) {
+							((MonsterMissile) ret).set(element.getAttackType(),
+									element.getStatusEffect(), element.getRange(),
+									element.getAttackMessage(), element.getEffectType(),
+									element.getEffectID(), element.getDamage(),
+									element.getEffectWav());
+						} else if (ret instanceof MonsterCharge) {
+							((MonsterCharge) ret).set(element.getRange(),
+									element.getAttackMessage(), element.getDamage(),
+									element.getEffectWav());
+						} else if (ret instanceof SummonMonster) {
+							((SummonMonster) ret).set(element.getSummonMonsterId(),
+									element.getAttackMessage());
 						}
-						else if ( ret instanceof MonsterCharge )
-						{
-							( (MonsterCharge) ret ).set( element.getRange( ),
-									element.getAttackMessage( ), element.getDamage( ),
-									element.getEffectWav( ) );
-						}
-						else if ( ret instanceof SummonMonster )
-						{
-							( (SummonMonster) ret ).set( element.getSummonMonsterId( ),
-									element.getAttackMessage( ) );
-						}
-						ret.setPosition( who.getLevel( ).getPlayer( ).getPosition( ) );
+						ret.setPosition(who.getLevel().getPlayer().getPosition());
 						return ret;
 					}
 				}
