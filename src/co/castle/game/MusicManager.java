7bc5e12e7f04dbdc79d50482a48a57d579c93012
Joan Andrés <andres6936@live.com>
2020-11-07 22:21:11 -0500

    Created new function for fill the hash music

--- src/co/castle/game/MusicManager.java
+++ src/co/castle/game/MusicManager.java
@@ -1,6 +1,8 @@
 package co.castle.game;
 
+import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Properties;
 
 import sz.midi.STMidiPlayer;
 import sz.mp3.JLayerMP3Player;
@@ -55,15 +57,43 @@ public class MusicManager {
 
 	// Method
 
+	/**
+	 * Load the list of track and path to it.
+	 * <p>
+	 * Post-condition: The Hash music has been filled with list of tracks
+	 * and paths to it.
+	 *
+	 * @param properties File with the key and path to tracks.
+	 */
+	public void addMusic(final Properties properties) {
+		Enumeration<?> keys = properties.keys();
+
+		while (keys.hasMoreElements()) {
+			final String key = (String) keys.nextElement();
+			// The properties file save the path of track with the prefix
+			// (aka. key) "mus_<KEY_TRACK>".
+			// Example of possibles keys:
+			// 	- mus_TITLE = "tracks/title.mp3"
+			// 	- mus_HIT = "effect/hit.mp3"
+			// Filter this keys.
+			if (key.startsWith("mus_")) {
+				// Deleted the part of "mus_" and use the rest of key name
+				// like key.
+				// Example: mus_TITLE will be TITLE
+				final String keyTrack = key.substring(4);
+				addMusic(keyTrack, properties.getProperty(key));
+			}
+		}
+	}
+
 	public void addMusic(String levelType, String fileName) {
 		musics.put(levelType, fileName);
 	}
 
 	public void die() {
 		STMidiPlayer.setInstruction(STMidiPlayer.INS_DIE);
-		if (currentMidiThread != null)
-		{
-			currentMidiThread.interrupt( );
+		if (currentMidiThread != null) {
+			currentMidiThread.interrupt();
 		}
 		JLayerMP3Player.setInstruction( JLayerMP3Player.INS_DIE );
 		if ( currentMP3Thread != null )
