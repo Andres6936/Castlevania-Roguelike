05833b284d49f80038da17399f89c00b468beabc
Joan Andrés <andres6936@live.com>
2021-04-11 09:47:29 -0500

    Updated documentation

--- src/co/castle/system/FileLoader.java
+++ src/co/castle/system/FileLoader.java
@@ -13,8 +13,8 @@ public class FileLoader {
      * @return Creates and return a FileInputStream by opening a connection to
      * an actual file, the file named by the path name name in the file system.
      * @throws FileNotFoundException If the file does not exist, is a directory
-     *                               rather than a regular file, or for some other reason cannot be opened
-     *                               for reading.
+     *                               rather than a regular file, or for some
+     *                               other reason cannot be opened for reading.
      */
     public static FileInputStream getFileInputStream(final String filename) throws FileNotFoundException {
         return new FileInputStream(filename);
