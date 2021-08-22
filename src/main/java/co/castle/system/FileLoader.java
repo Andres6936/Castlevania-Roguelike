7bf3824715ab83343dfe7a0e4e165f70bd2edd65
Joan Andrés <andres6936@live.com>
2021-08-22 11:38:50 -0500

    Removed unused methods

--- src/main/java/co/castle/system/FileLoader.java
+++ src/main/java/co/castle/system/FileLoader.java
@@ -47,18 +47,6 @@ public class FileLoader {
     }
 
     /**
-     * @param filename The system-dependent file name.
-     * @return Creates and return a FileInputStream by opening a connection to
-     * an actual file, the file named by the path name name in the file system.
-     * @throws FileNotFoundException If the file does not exist, is a directory
-     *                               rather than a regular file, or for some
-     *                               other reason cannot be opened for reading.
-     */
-    public static FileInputStream getFileInputStream(final String filename) throws FileNotFoundException {
-        return new FileInputStream(ClassLoader.getSystemResource(filename).getFile());
-    }
-
-    /**
      * @param filename The file to be opened for reading.
      * @return Creates and return a FileInputStream by opening a connection to
      * an actual file, the file named by the path name name in the file system.
