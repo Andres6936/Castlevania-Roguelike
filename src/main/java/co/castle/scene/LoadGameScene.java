4b4e2b78eec626801aca234473d5fbc5024113ba
Joan Andrés <andres6936@live.com>
2021-08-28 15:41:01 -0500

    Created Scenes Class

---
+++ src/main/java/co/castle/scene/LoadGameScene.java
@@ -0,0 +1,18 @@
+package co.castle.scene;
+
+public class LoadGameScene implements IScene {
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public TypeScene process() {
+        return null;
+    }
+}
---
+++ src/main/java/co/castle/scene/TrainingScene.java
@@ -0,0 +1,18 @@
+package co.castle.scene;
+
+public class TrainingScene implements IScene {
+    @Override
+    public void draw() {
+
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public TypeScene process() {
+        return null;
+    }
+}
