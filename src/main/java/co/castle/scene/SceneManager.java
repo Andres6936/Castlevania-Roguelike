c05e2bffbcfd8256a0299cc86ed652b42132e6b8
Joan Andrés <andres6936@live.com>
2021-08-28 15:47:26 -0500

    Prepare Scene Manager

--- src/main/java/co/castle/scene/SceneManager.java
+++ src/main/java/co/castle/scene/SceneManager.java
@@ -1,15 +1,40 @@
 package co.castle.scene;
 
 public class SceneManager {
+
+    /**
+     * True while the app is running, false when the user want exit.
+     */
     private boolean running = true;
+
+    /**
+     * Point to the current Scene.
+     */
     private IScene current;
 
-    public void draw() {
+    /**
+     * Is always the first scene that is renderer.
+     */
+    private final IScene menuScene;
 
+    private IScene arenaScene;
+    private IScene newGameScene;
+    private IScene loadGameScene;
+    private IScene prologueScene;
+    private IScene trainingScene;
+
+    public SceneManager() {
+        menuScene = new MenuScene();
+        // The first scene is the Menu.
+        current = menuScene;
+    }
+
+    public void draw() {
+        current.draw();
     }
 
     public void update() {
-
+        current.update();
     }
 
     public void process() {
