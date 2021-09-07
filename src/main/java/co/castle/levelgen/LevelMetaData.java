fcc5fc993c423ca9c1ae0827cf668b240698caa6
Joan Andrés <andres6936@live.com>
2021-09-06 19:35:13 -0500

    Removed unused code

--- src/main/java/co/castle/levelgen/LevelMetaData.java
+++ src/main/java/co/castle/levelgen/LevelMetaData.java
@@ -6,30 +6,18 @@ import java.util.Vector;
 
 public class LevelMetaData implements Serializable
 {
-	private Vector exits = new Vector( );
-	private Hashtable hexits = new Hashtable( );
+	private final Hashtable<String, String> hexits = new Hashtable<>();
 	private String levelID;
 	private int levelNumber = -1;
 
 	public void addExits( String exit, String exitID )
 	{
-		exits.add( exit );
 		hexits.put( exitID, exit );
-	}
-
-	public String getExit( int number )
-	{
-		return (String) exits.elementAt( number );
 	}
 
 	public String getExit( String exitID )
 	{
-		return (String) hexits.get( exitID );
-	}
-
-	public Vector getExits( )
-	{
-		return exits;
+		return hexits.get(exitID);
 	}
 
 	public String getLevelID( )
