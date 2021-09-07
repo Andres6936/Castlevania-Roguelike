f312c197de782dceb721e9b63c2383619be226a3
Joan Andrés <andres6936@live.com>
2021-09-06 19:42:23 -0500

    Display intro

--- src/main/java/co/castle/scene/NewGameScene.java
+++ src/main/java/co/castle/scene/NewGameScene.java
@@ -5,6 +5,7 @@ import co.castle.game.PlayerGenerator;
 import co.castle.player.GameSessionInfo;
 import co.castle.player.Player;
 import co.castle.player.PlayerEventListener;
+import co.castle.ui.Display;
 import co.castle.ui.UISelector;
 import co.castle.ui.UserInterface;
 
@@ -34,12 +35,13 @@ public class NewGameScene implements IScene {
 
             }
         });
-        userInterface.setPlayer(player);
         selector.setPlayer(player);
+        userInterface.setPlayer(player);
         userInterface.setGameOver(false);
         userInterface.addCommandListener(pCommand -> {
 
         });
+        Display.thus.showIntro(player);
     }
 
     @Override
