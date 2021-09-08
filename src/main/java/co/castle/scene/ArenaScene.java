c5804f6c42d6bb05370e47353b554d0bf125b9f1
Joan Andrés <andres6936@live.com>
2021-09-08 18:01:46 -0500

    Delete duplicate code

--- src/main/java/co/castle/scene/ArenaScene.java
+++ src/main/java/co/castle/scene/ArenaScene.java
@@ -1,13 +1,6 @@
 package co.castle.scene;
 
-import co.castle.game.CRLException;
-import co.castle.game.MusicManager;
-import co.castle.item.Item;
-import co.castle.level.Level;
-import co.castle.levelgen.LevelMaster;
 import co.castle.levelgen.LevelMetaData;
-import co.castle.npc.Hostage;
-import co.castle.ui.Display;
 import co.castle.ui.UISelector;
 import sz.fov.FOV;
 
@@ -20,95 +13,7 @@ public class ArenaScene extends GenericScene implements IScene {
         md.setLevelID("PRELUDE_ARENA");
         levelMetadata.put("PRELUDE_ARENA", md);
 
-        String levelID = "PRELUDE_ARENA";
-        String formerLevelID;
-        if (currentLevel != null) {
-            if (currentLevel.getBoss() != null && !currentLevel.getBoss().isDead())
-                return;
-            formerLevelID = currentLevel.getID();
-            Level storedLevel = storedLevels.get(formerLevelID);
-            if (storedLevel == null) {
-                storedLevels.put(formerLevelID, currentLevel);
-            }
-        } else {
-            formerLevelID = "_BACK";
-        }
-        Level storedLevel = storedLevels.get(levelID);
-        if (storedLevel != null) {
-            currentLevel = storedLevel;
-            player.setPosition(currentLevel.getExitFor(formerLevelID));
-            currentLevel.setIsDay(isDay);
-            currentLevel.setTimecounter(timeSwitch);
-            if (currentLevel.isCandled()) {
-                currentLevel.destroyCandles();
-                LevelMaster.lightCandles(currentLevel);
-            }
-        } else {
-            try {
-                currentLevel = LevelMaster.createLevel(
-                        levelMetadata.get(levelID), player);
-                currentLevel.setPlayer(player);
-                userInterface.setPlayer(player);
-                selector.setPlayer(player);
-                currentLevel.setIsDay(isDay);
-                currentLevel.setTimecounter(timeSwitch);
-                if (currentLevel.getPlayer() != null)
-                    currentLevel.getPlayer().addHistoricEvent(
-                            "got to the " + currentLevel.getDescription());
-            } catch (CRLException crle) {
-                System.err.println("Error while creating level ");
-                crle.printStackTrace();
-                System.exit(-100);
-            }
-        }
-        // currentLevel.setLevelNumber(targetLevelNumber);
-        player.setLevel(currentLevel);
-
-        if (currentLevel.getExitFor(formerLevelID) != null) {
-            player.setPosition(currentLevel.getExitFor(formerLevelID));
-        } else if (currentLevel.getExitFor("_START") != null) {
-            player.setPosition(currentLevel.getExitFor("_START"));
-        }
-
-        if (currentLevel.isHostageSafe()) {
-            if (player.hasHostage()) {
-                // player.setPosition(currentLevel.getExitFor("_NEXT"));
-                Hostage h = player.getHostage();
-                player.setHostage(null);
-                h.setPosition(player.getPosition().x - 3, player.getPosition().y,
-                        player.getPosition().z);
-                h.setRescued(true);
-                currentLevel.addMonster(h);
-                player.addHistoricEvent(
-                        "brought " + h.getDescription() + " to safety");
-                Display.thus.showHostageRescue(h);
-                player.addGold(h.getReward());
-                Item reward = h.getItemReward();
-                if (reward != null)
-                    if (player.canCarry())
-                        player.addItem(reward);
-                    else
-                        player.getLevel().addItem(player.getPosition(), reward);
-            }
-        }
-        dispatcher = currentLevel.getDispatcher();
-        if (currentLevel.hasNoonMusic() && !currentLevel.isDay()) {
-            MusicManager.playKey(currentLevel.getMusicKeyNoon());
-        } else {
-            MusicManager.playKey(currentLevel.getMusicKeyMorning());
-        }
-        if (currentLevel.isRutinary()) {
-            currentLevel.anihilateMonsters();
-            currentLevel.populate();
-        }
-
-        if (currentLevel.getBoss() != null) {
-            currentLevel.getBoss().recoverHits();
-        }
-        if (!dispatcher.contains(player)) {
-            dispatcher.addActor(player);
-        }
-        userInterface.levelChange();
+        loadLevel("PRELUDE_ARENA");
 
         currentLevel.setIsDay(false);
         turns = 0;
--- src/main/java/co/castle/scene/GenericScene.java
+++ src/main/java/co/castle/scene/GenericScene.java
@@ -1,11 +1,15 @@
 package co.castle.scene;
 
 import co.castle.actor.Actor;
+import co.castle.game.CRLException;
 import co.castle.game.MusicManager;
 import co.castle.game.PlayerGenerator;
+import co.castle.item.Item;
 import co.castle.level.Dispatcher;
 import co.castle.level.Level;
 import co.castle.level.RepositoryLevelMetadata;
+import co.castle.levelgen.LevelMaster;
+import co.castle.npc.Hostage;
 import co.castle.player.Consts;
 import co.castle.player.GameSessionInfo;
 import co.castle.player.Player;
@@ -97,6 +101,97 @@ public class GenericScene {
             timeSwitch = DAY_LENGTH;
             currentLevel.setTimecounter(timeSwitch);
         }
+    }
+
+    protected void loadLevel(String levelID) {
+        String formerLevelID;
+        if (currentLevel != null) {
+            if (currentLevel.getBoss() != null && !currentLevel.getBoss().isDead())
+                return;
+            formerLevelID = currentLevel.getID();
+            Level storedLevel = storedLevels.get(formerLevelID);
+            if (storedLevel == null) {
+                storedLevels.put(formerLevelID, currentLevel);
+            }
+        } else {
+            formerLevelID = "_BACK";
+        }
+        Level storedLevel = storedLevels.get(levelID);
+        if (storedLevel != null) {
+            currentLevel = storedLevel;
+            player.setPosition(currentLevel.getExitFor(formerLevelID));
+            currentLevel.setIsDay(isDay);
+            currentLevel.setTimecounter(timeSwitch);
+            if (currentLevel.isCandled()) {
+                currentLevel.destroyCandles();
+                LevelMaster.lightCandles(currentLevel);
+            }
+        } else {
+            try {
+                currentLevel = LevelMaster.createLevel(
+                        levelMetadata.get(levelID), player);
+                currentLevel.setPlayer(player);
+                userInterface.setPlayer(player);
+                selector.setPlayer(player);
+                currentLevel.setIsDay(isDay);
+                currentLevel.setTimecounter(timeSwitch);
+                if (currentLevel.getPlayer() != null)
+                    currentLevel.getPlayer().addHistoricEvent(
+                            "got to the " + currentLevel.getDescription());
+            } catch (CRLException crle) {
+                System.err.println("Error while creating level ");
+                crle.printStackTrace();
+                System.exit(-100);
+            }
+        }
+        // currentLevel.setLevelNumber(targetLevelNumber);
+        player.setLevel(currentLevel);
+
+        if (currentLevel.getExitFor(formerLevelID) != null) {
+            player.setPosition(currentLevel.getExitFor(formerLevelID));
+        } else if (currentLevel.getExitFor("_START") != null) {
+            player.setPosition(currentLevel.getExitFor("_START"));
+        }
+
+        if (currentLevel.isHostageSafe()) {
+            if (player.hasHostage()) {
+                // player.setPosition(currentLevel.getExitFor("_NEXT"));
+                Hostage h = player.getHostage();
+                player.setHostage(null);
+                h.setPosition(player.getPosition().x - 3, player.getPosition().y,
+                        player.getPosition().z);
+                h.setRescued(true);
+                currentLevel.addMonster(h);
+                player.addHistoricEvent(
+                        "brought " + h.getDescription() + " to safety");
+                Display.thus.showHostageRescue(h);
+                player.addGold(h.getReward());
+                Item reward = h.getItemReward();
+                if (reward != null)
+                    if (player.canCarry())
+                        player.addItem(reward);
+                    else
+                        player.getLevel().addItem(player.getPosition(), reward);
+            }
+        }
+        dispatcher = currentLevel.getDispatcher();
+        if (currentLevel.hasNoonMusic() && !currentLevel.isDay()) {
+            MusicManager.playKey(currentLevel.getMusicKeyNoon());
+        } else {
+            MusicManager.playKey(currentLevel.getMusicKeyMorning());
+        }
+        if (currentLevel.isRutinary()) {
+            currentLevel.anihilateMonsters();
+            currentLevel.populate();
+        }
+
+        if (currentLevel.getBoss() != null) {
+            currentLevel.getBoss().recoverHits();
+        }
+        if (!dispatcher.contains(player)) {
+            dispatcher.addActor(player);
+        }
+        userInterface.levelChange();
     }
 
     protected void update() {
