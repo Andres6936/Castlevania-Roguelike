ef26147f0a5c02d4bc662c9548a3632076a6e0f4
Joan Andrés <andres6936@live.com>
2021-09-08 14:44:08 -0500

    Avoid repeat code

--- src/main/java/co/castle/scene/ArenaScene.java
+++ src/main/java/co/castle/scene/ArenaScene.java
@@ -23,44 +23,10 @@ import sz.util.Util;
 
 import java.util.Hashtable;
 
-public class ArenaScene implements IScene {
-    public final static int DAY_LENGTH = 500;
-    private final Player player;
-    private final UISelector selector;
-    private final UserInterface userInterface;
-    private final Hashtable<String, Level> storedLevels = new Hashtable<>();
-    private final RepositoryLevelMetadata levelMetadata = new RepositoryLevelMetadata();
-
-    private int timeSwitch;
-    private long turns;
-    private boolean isDay = true;
-    private Level currentLevel;
-    private Dispatcher dispatcher;
+public class ArenaScene extends GenericScene implements IScene {
 
     public ArenaScene(UISelector selector) {
-        this.selector = selector;
-        userInterface = UserInterface.getUI();
-        player = PlayerGenerator.thus.createSpecialPlayer("SONIA");
-        player.setGameSessionInfo(new GameSessionInfo());
-        player.setSelector(selector);
-        player.setDoNotRecordScore(false);
-        selector.setPlayer(player);
-        userInterface.setPlayer(player);
-        userInterface.setGameOver(false);
-        userInterface.addCommandListener(e -> {
-
-        });
-        player.setPlayerEventListener(new PlayerEventListener() {
-            @Override
-            public void informEvent(int code) {
-
-            }
-
-            @Override
-            public void informEvent(int code, Object param) {
-
-            }
-        });
+        super(selector, "SONIA");
 
         LevelMetaData md = new LevelMetaData();
         md.setLevelID("PRELUDE_ARENA");
@@ -167,49 +133,6 @@ public class ArenaScene implements IScene {
         checkTimeSwitch();
     }
 
-    private void checkTimeSwitch() {
-        timeSwitch--;
-        currentLevel.setTimecounter(timeSwitch);
-        if (timeSwitch <= 0) {
-            // Environmental Effects, random
-            boolean rain = Util.chance(20);
-            boolean thunderstorm = !rain && Util.chance(10);
-            boolean fog = !rain && !thunderstorm && Util.chance(10);
-            boolean sunnyDay = !isDay && !fog && !rain && !thunderstorm
-                    && Util.chance(20);
-
-            player.setFlag(Consts.ENV_RAIN, rain);
-            player.setFlag(Consts.ENV_THUNDERSTORM, thunderstorm);
-            player.setFlag(Consts.ENV_FOG, fog);
-            player.setFlag(Consts.ENV_SUNNY, sunnyDay);
-
-            if (isDay) {
-                if (currentLevel.hasNoonMusic()) {
-                    MusicManager.stopMusic();
-                    Display.thus.showTimeChange(!isDay, fog, rain, thunderstorm, false);
-                    MusicManager.playKey(currentLevel.getMusicKeyNoon());
-                } else {
-                    Display.thus.showTimeChange(!isDay, fog, rain, thunderstorm, false);
-                }
-            } else {
-
-                if (currentLevel.hasNoonMusic()) {
-                    MusicManager.stopMusic();
-                    Display.thus.showTimeChange(!isDay, fog, rain, thunderstorm,
-                            sunnyDay);
-                    MusicManager.playKey(currentLevel.getMusicKeyMorning());
-                } else {
-                    Display.thus.showTimeChange(!isDay, fog, rain, thunderstorm,
-                            sunnyDay);
-                }
-            }
-            isDay = !isDay;
-            currentLevel.setIsDay(isDay);
-            timeSwitch = DAY_LENGTH;
-            currentLevel.setTimecounter(timeSwitch);
-        }
-    }
-
     @Override
     public void draw() {
 
@@ -217,28 +140,7 @@ public class ArenaScene implements IScene {
 
     @Override
     public void update() {
-        Actor actor = dispatcher.getNextActor();
-        if (actor == player) {
-            player.darken();
-            player.see();
-            if (!player.justJumped())
-                userInterface.refresh();
-            player.getGameSessionInfo().increaseTurns();
-            player.checkDeath();
-//            player.getLevel( ).checkUnleashers( this );
-
-        }
-        actor.act();
-        actor.getLevel().getDispatcher().returnActor(actor);
-
-        if (actor == player) {
-            if (currentLevel != null)
-                currentLevel.updateLevelStatus();
-            // ui.refresh();
-            turns++;
-            // player.addScore(1);
-            checkTimeSwitch();
-        }
+        super.update();
     }
 
     @Override
--- src/main/java/co/castle/scene/TrainingScene.java
+++ src/main/java/co/castle/scene/TrainingScene.java
@@ -2,64 +2,19 @@ package co.castle.scene;
 
 import co.castle.game.CRLException;
 import co.castle.game.MusicManager;
-import co.castle.game.PlayerGenerator;
 import co.castle.item.Item;
-import co.castle.level.Dispatcher;
 import co.castle.level.Level;
-import co.castle.level.RepositoryLevelMetadata;
 import co.castle.levelgen.LevelMaster;
 import co.castle.levelgen.LevelMetaData;
 import co.castle.npc.Hostage;
-import co.castle.player.Consts;
-import co.castle.player.GameSessionInfo;
-import co.castle.player.Player;
-import co.castle.player.PlayerEventListener;
 import co.castle.ui.Display;
 import co.castle.ui.UISelector;
-import co.castle.ui.UserInterface;
 import sz.fov.FOV;
-import sz.util.Util;
 
-import java.util.Hashtable;
-
-public class TrainingScene implements IScene {
-    public final static int DAY_LENGTH = 500;
-    private final Player player;
-    private final UISelector selector;
-    private final UserInterface userInterface;
-    private final Hashtable<String, Level> storedLevels = new Hashtable<>();
-    private final RepositoryLevelMetadata levelMetadata = new RepositoryLevelMetadata();
-
-    private int timeSwitch;
-    private long turns;
-    private boolean isDay = true;
-    private Level currentLevel;
-    private Dispatcher dispatcher;
+public class TrainingScene extends GenericScene implements IScene {
 
     public TrainingScene(UISelector selector) {
-        this.selector = selector;
-        userInterface = UserInterface.getUI();
-        player = PlayerGenerator.thus.createSpecialPlayer("SOLIEYU_KID");
-        player.setGameSessionInfo(new GameSessionInfo());
-        player.setSelector(selector);
-        player.setDoNotRecordScore(true);
-        selector.setPlayer(player);
-        userInterface.setPlayer(player);
-        userInterface.setGameOver(false);
-        userInterface.addCommandListener(e -> {
-
-        });
-        player.setPlayerEventListener(new PlayerEventListener() {
-            @Override
-            public void informEvent(int code) {
-
-            }
-
-            @Override
-            public void informEvent(int code, Object param) {
-
-            }
-        });
+        super(selector, "SOLIEYU_KID");
 
         LevelMetaData md = new LevelMetaData();
         md.setLevelID("TRAINING");
@@ -164,49 +119,6 @@ public class TrainingScene implements IScene {
         checkTimeSwitch();
     }
 
-    private void checkTimeSwitch() {
-        timeSwitch--;
-        currentLevel.setTimecounter(timeSwitch);
-        if (timeSwitch <= 0) {
-            // Environmental Effects, random
-            boolean rain = Util.chance(20);
-            boolean thunderstorm = !rain && Util.chance(10);
-            boolean fog = !rain && !thunderstorm && Util.chance(10);
-            boolean sunnyDay = !isDay && !fog && !rain && !thunderstorm
-                    && Util.chance(20);
-
-            player.setFlag(Consts.ENV_RAIN, rain);
-            player.setFlag(Consts.ENV_THUNDERSTORM, thunderstorm);
-            player.setFlag(Consts.ENV_FOG, fog);
-            player.setFlag(Consts.ENV_SUNNY, sunnyDay);
-
-            if (isDay) {
-                if (currentLevel.hasNoonMusic()) {
-                    MusicManager.stopMusic();
-                    Display.thus.showTimeChange(!isDay, fog, rain, thunderstorm, false);
-                    MusicManager.playKey(currentLevel.getMusicKeyNoon());
-                } else {
-                    Display.thus.showTimeChange(!isDay, fog, rain, thunderstorm, false);
-                }
-            } else {
-
-                if (currentLevel.hasNoonMusic()) {
-                    MusicManager.stopMusic();
-                    Display.thus.showTimeChange(!isDay, fog, rain, thunderstorm,
-                            sunnyDay);
-                    MusicManager.playKey(currentLevel.getMusicKeyMorning());
-                } else {
-                    Display.thus.showTimeChange(!isDay, fog, rain, thunderstorm,
-                            sunnyDay);
-                }
-            }
-            isDay = !isDay;
-            currentLevel.setIsDay(isDay);
-            timeSwitch = DAY_LENGTH;
-            currentLevel.setTimecounter(timeSwitch);
-        }
-    }
-
     @Override
     public void draw() {
 
@@ -214,7 +126,7 @@ public class TrainingScene implements IScene {
 
     @Override
     public void update() {
-
+        super.update();
     }
 
     @Override
