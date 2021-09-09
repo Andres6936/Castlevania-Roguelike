80a600c13d32d9c8d8dbb78f316ab0913eeb70fd
Joan Andrés <andres6936@live.com>
2021-09-09 12:04:15 -0500

    Removed unused code

--- src/main/java/co/castle/game/Game.java
+++ src/main/java/co/castle/game/Game.java
@@ -1,12 +1,7 @@
 package co.castle.game;
 
-import co.castle.item.Item;
 import co.castle.level.Dispatcher;
 import co.castle.level.Level;
-import co.castle.level.RepositoryLevelMetadata;
-import co.castle.levelgen.LevelMaster;
-import co.castle.npc.Hostage;
-import co.castle.player.Consts;
 import co.castle.player.GameSessionInfo;
 import co.castle.player.Player;
 import co.castle.player.PlayerEventListener;
@@ -15,32 +10,17 @@ import co.castle.ui.Display;
 import co.castle.ui.UISelector;
 import co.castle.ui.UserInterface;
 import sz.fov.FOV;
-import sz.util.Debug;
-import sz.util.Util;
 
-import java.util.Hashtable;
 import java.util.Vector;
 
 public class Game implements CommandListener, PlayerEventListener, java.io.Serializable {
 	private boolean canSave;
 	private Level currentLevel;
 
-	private final String[] DEATHMESSAGES = new String[]
-			{"You are dead... and Dracula is still alive", "All hopes are lost.",
-					"It's the end.",
-					"Let us enjoy this evening for pleasure, the night is still young...",
-					"Game Over", "Better luck next time, Son of a Belmont"};
 	private Dispatcher dispatcher;
-	private boolean endGame;
-	private boolean isDay = true;
-
-	private final RepositoryLevelMetadata levelMetadata = new RepositoryLevelMetadata();
-	// private String[] levelPath;
 
 	private Player player;
 
-	private Hashtable<String, Level> /* Level */ storedLevels = new Hashtable<>();
-	private int timeSwitch;
 	private long turns;
 	// Configuration
 	private transient UserInterface ui;
@@ -83,7 +63,6 @@ public class Game implements CommandListener, PlayerEventListener, java.io.Seria
 		ui.setGameOver( true );
 		player.setPlayerEventListener( null );
 
-		endGame = true;
 	}
 
 	public Player getPlayer( )
@@ -141,60 +120,6 @@ public class Game implements CommandListener, PlayerEventListener, java.io.Seria
 		finishGame( );
 	}
 
-	private void checkTimeSwitch( )
-	{
-		if ( endGame )
-			return;
-		timeSwitch--;
-		currentLevel.setTimecounter( timeSwitch );
-		if ( timeSwitch <= 0 )
-		{
-			// Environmental Effects, random
-			boolean rain = Util.chance( 20 );
-			boolean thunderstorm = !rain && Util.chance( 10 );
-			boolean fog = !rain && !thunderstorm && Util.chance( 10 );
-			boolean sunnyDay = !isDay && !fog && !rain && !thunderstorm
-					&& Util.chance( 20 );
-
-			player.setFlag( Consts.ENV_RAIN, rain );
-			player.setFlag( Consts.ENV_THUNDERSTORM, thunderstorm );
-			player.setFlag( Consts.ENV_FOG, fog );
-			player.setFlag( Consts.ENV_SUNNY, sunnyDay );
-
-			if ( isDay )
-			{
-				if ( currentLevel.hasNoonMusic( ) ) {
-					MusicManager.stopMusic();
-					Display.thus.showTimeChange(!isDay, fog, rain, thunderstorm, false);
-					MusicManager.playKey(currentLevel.getMusicKeyNoon());
-				}
-				else
-				{
-					Display.thus.showTimeChange( !isDay, fog, rain, thunderstorm, false );
-				}
-			}
-			else
-			{
-
-				if ( currentLevel.hasNoonMusic( ) ) {
-					MusicManager.stopMusic();
-					Display.thus.showTimeChange(!isDay, fog, rain, thunderstorm,
-							sunnyDay);
-					MusicManager.playKey(currentLevel.getMusicKeyMorning());
-				}
-				else
-				{
-					Display.thus.showTimeChange( !isDay, fog, rain, thunderstorm,
-							sunnyDay );
-				}
-			}
-			isDay = !isDay;
-			currentLevel.setIsDay( isDay );
-			timeSwitch = DAY_LENGTH;
-			currentLevel.setTimecounter( timeSwitch );
-		}
-	}
-
 	private void finishGame( )
 	{
 		if ( !player.isDoNotRecordScore( ) )
@@ -211,122 +136,6 @@ public class Game implements CommandListener, PlayerEventListener, java.io.Seria
 		exitGame( );
 	}
 
-	private void forwardTime( )
-	{
-		timeSwitch = 0;
-		checkTimeSwitch( );
-	}
-
-
-	private void loadLevel( String levelID )
-	{
-		loadLevel( levelID, -1 );
-	}
-
-	private void loadLevel( String levelID, int targetLevelNumber ) {
-		Debug.enterMethod(this, "loadLevel", levelID + "," + targetLevelNumber);
-		String formerLevelID;
-		if (currentLevel != null) {
-			if (currentLevel.getBoss() != null && !currentLevel.getBoss().isDead())
-				return;
-			formerLevelID = currentLevel.getID();
-			Level storedLevel = storedLevels.get(formerLevelID);
-			if (storedLevel == null) {
-				storedLevels.put(formerLevelID, currentLevel);
-			}
-		} else {
-			formerLevelID = "_BACK";
-		}
-		Level storedLevel = storedLevels.get(levelID);
-		if (storedLevel != null) {
-			currentLevel = storedLevel;
-			player.setPosition(currentLevel.getExitFor(formerLevelID));
-			currentLevel.setIsDay(isDay);
-			currentLevel.setTimecounter(timeSwitch);
-			if (currentLevel.isCandled()) {
-				currentLevel.destroyCandles();
-				LevelMaster.lightCandles(currentLevel);
-			}
-		}
-		else
-		{
-			try
-			{
-				currentLevel = LevelMaster.createLevel(
-						levelMetadata.get(levelID), player);
-				currentLevel.setPlayer(player);
-				ui.setPlayer( player );
-				uiSelector.setPlayer( player );
-				currentLevel.setIsDay( isDay );
-				currentLevel.setTimecounter( timeSwitch );
-				if ( currentLevel.getPlayer( ) != null )
-					currentLevel.getPlayer( ).addHistoricEvent(
-							"got to the " + currentLevel.getDescription( ) );
-			}
-			catch ( CRLException crle )
-			{
-				System.err.println("Error while creating level " + levelID);
-			}
-		}
-		// currentLevel.setLevelNumber(targetLevelNumber);
-		player.setLevel( currentLevel );
-
-		if ( currentLevel.getExitFor( formerLevelID ) != null )
-		{
-			player.setPosition( currentLevel.getExitFor( formerLevelID ) );
-		}
-		else if ( currentLevel.getExitFor( "_START" ) != null )
-		{
-			player.setPosition( currentLevel.getExitFor( "_START" ) );
-		}
-
-		if ( currentLevel.isHostageSafe( ) )
-		{
-			if ( player.hasHostage( ) )
-			{
-				// player.setPosition(currentLevel.getExitFor("_NEXT"));
-				Hostage h = player.getHostage( );
-				player.setHostage( null );
-				h.setPosition( player.getPosition( ).x - 3, player.getPosition( ).y,
-						player.getPosition( ).z );
-				h.setRescued( true );
-				currentLevel.addMonster( h );
-				player.addHistoricEvent(
-						"brought " + h.getDescription( ) + " to safety" );
-				Display.thus.showHostageRescue( h );
-				player.addGold( h.getReward( ) );
-				Item reward = h.getItemReward( );
-				if ( reward != null )
-					if ( player.canCarry( ) )
-						player.addItem( reward );
-					else
-						player.getLevel( ).addItem( player.getPosition( ), reward );
-			}
-		}
-		dispatcher = currentLevel.getDispatcher( );
-		if ( currentLevel.hasNoonMusic( ) && !currentLevel.isDay( ) ) {
-			MusicManager.playKey(currentLevel.getMusicKeyNoon());
-		}
-		else {
-			MusicManager.playKey(currentLevel.getMusicKeyMorning());
-		}
-		if ( currentLevel.isRutinary( ) )
-		{
-			currentLevel.anihilateMonsters( );
-			currentLevel.populate( );
-		}
-
-		if ( currentLevel.getBoss( ) != null )
-		{
-			currentLevel.getBoss( ).recoverHits( );
-		}
-		if ( !dispatcher.contains( player ) )
-		{
-			dispatcher.addActor( player );
-		}
-		ui.levelChange( );
-		Debug.exitMethod( );
-	}
 
 	private void resumeScreen( ) {
 		MusicManager.playKey("GAME_OVER");
